bc2f41495f2de0d012db3f016a592f7d850d057f
Ali Hasan <alihasan@Ali-Hasans-Mac-mini-2.local>
2011-04-12 20:28:25 +0500

    Initial Commit

---
+++ iBeamBack/iBeamBackAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  iBeamBackAppDelegate.h
+//  iBeamBack
+//
+//  Created by Ali Hasan on 4/12/11.
+//  Copyright 2011 Scrybe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iBeamBackAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ iBeamBack/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iBeamBack
+//
+//  Created by Ali Hasan on 4/12/11.
+//  Copyright 2011 Scrybe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
